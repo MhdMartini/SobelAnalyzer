1aa5374ab5764fcba09ff6ab98921e3763a8a582
MhdMartini <mohamed_martini@student.uml.edu>
2021-04-02 21:11:44 -0400

    tested

--- sobelAnalyzer.c
+++ sobelAnalyzer.c
@@ -4,11 +4,12 @@
 #include <stdbool.h>
 #include "imgIo.h"
 #include "imgOp.h"
+#include "sobel.h"
 
 int main(){
     // unsigned * shape = get_size(imgSobelPath);
     // unsigned sizeX = shape[0];
     // unsigned sizeY = shape[1];
-
+    imgSobel("test_images/cake.pgm", 55, 1, 256, 256);
     return 0;
 }
